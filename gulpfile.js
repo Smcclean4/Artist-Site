fdb58826f7551b4456c6146d072f08611bbd1ca2
Sidney McClean <sidney.mcclean@outlook.com>
2019-01-09 22:08:12 -0800

    Gulp edits

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,16 @@
 const gulp = require('gulp');
-const css = require('cssnano');
-const rename = require('gulp-rename');
+const browserSync = require('browser-sync').create();
+const cssnano = require('cssnano');
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+const rename = require('gulp')
+const gulpIf = require('gulp-if');
 
-function cssMin(done) {
-  return src('project/**/*.css')
-      .pipe(css())
-      .pipe(rename({extname: '.min.css'}))
-      .pipe(dest('dist'))
+function browser_sync(done) {
+  browserSync.init( {
+    server: {
+      baseDir: 'project'
+    }
+  });
   done();
 }
-
-var watcher = watch('project/**/*.css', cssMin);
-
-exports.watch = watcher;
