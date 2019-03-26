636caca4f86148353560f4038e73efabd233cd89
Sidney McClean <sidney.mcclean@outlook.com>
2019-03-26 16:53:00 -0700

    built out banner section

--- project/js/script.js
+++ project/js/script.js
@@ -1,16 +1,3 @@
 $(document).ready(function() {
-var div = $('#divToToggle');
-var pos = div.position();
-
-$(window).scroll(funciton() {
-  var windowpos = $(window).scrollTop();
-  console.log(pos.top)
-  if (windowpos > pos.top && pos.top+500 > windowpos) {
-    div.addClass('AfterScroll');
-      div.removeClass("beforeScroll");
-  } else {
-    div.addClass('BeforeScroll');
-    div.removeClass('AfterScroll');
-  }
-});
+  
 });
