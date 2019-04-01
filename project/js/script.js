73b42c996fea17d7739fc17508abe75b65a6d0d3
Sidney McClean <sidney.mcclean@outlook.com>
2019-03-31 18:16:39 -0700

    images

--- project/js/script.js
+++ project/js/script.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
-  
+
 });
+
+const images = ['https://i.imgur.com/8T9VSVa.jpg', 'https://i.imgur.com/erkPzsj.jpg', 'https://i.imgur.com/sJcD6J9.jpg', 'https://i.imgur.com/6oL7z0o.jpg',
+'https://i.imgur.com/hyoXpef.jpg', 'https://i.imgur.com/U3KngPK.jpg'];
+const bannerImg = document.getElementById('banner');
+const randoImage = Math.floor((Math.random() * images.length - 1) + 1);
+// getting bannerImg initial images
+bannerImg.style.backgroundImage = 'url(' + images[randoImage] + ')';
