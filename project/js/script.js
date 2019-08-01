6511ea4c4267f308e6b74ed527e6775a2e71f5a2
Sidney McClean <sidney.mcclean@outlook.com>
2019-08-01 12:40:39 -0700

    editing to banner styling

--- project/js/script.js
+++ project/js/script.js
@@ -4,7 +4,11 @@ $(document).ready(function() {
 
 const images = ['https://i.imgur.com/8T9VSVa.jpg', 'https://i.imgur.com/erkPzsj.jpg', 'https://i.imgur.com/sJcD6J9.jpg', 'https://i.imgur.com/6oL7z0o.jpg',
 'https://i.imgur.com/hyoXpef.jpg', 'https://i.imgur.com/U3KngPK.jpg'];
-const bannerImg = document.getElementById('banner');
-const randoImage = Math.floor((Math.random() * images.length - 1) + 1);
-// getting bannerImg initial images
-bannerImg.style.backgroundImage = 'url(' + images[randoImage] + ')';
+const randoImg = Math.round(Math.random() * images.length);
+const bannerImg = document.getElementById('banner').style.backgroundImage = 'url(' + images[randoImg] + ')';
+
+function shuffle() {
+    
+}
+
+shuffle();
